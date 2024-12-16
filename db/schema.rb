3832091bc6bdf8df785cac879bbf37ddb975c2ec
Shreyas <shreyas314159@gmail.com>
2024-12-16 06:43:08 +0000

    rails g model StakeholderUpdate title content project:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_16_064059) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_16_064243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -106,6 +106,15 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_064059) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "stakeholder_updates", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_stakeholder_updates_on_project_id"
+  end
+
   create_table "user_submissions", force: :cascade do |t|
     t.string "email"
     t.string "first_name"
@@ -140,4 +149,5 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_064059) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "projects", "users"
+  add_foreign_key "stakeholder_updates", "projects"
 end
