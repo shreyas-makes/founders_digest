c42705da3f6866b4a13b869c1242220b2ad4c282
Shreyas <shreyas314159@gmail.com>
2024-12-16 06:41:30 +0000

    added Project model
    rails g model Project title website description avatar_url user:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_16_063623) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_16_064059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -87,6 +87,17 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_063623) do
     t.index ["user_id"], name: "index_mail_logs_on_user_id"
   end
 
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.string "website"
+    t.string "description"
+    t.string "avatar_url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
   create_table "script_tags", force: :cascade do |t|
     t.string "name"
     t.string "code"
@@ -128,4 +139,5 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_063623) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "projects", "users"
 end
