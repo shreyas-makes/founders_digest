d466003a545826b50497a21863b181ff728301df
Shreyas <shreyas314159@gmail.com>
2024-12-22 12:57:55 +0000

    added validations to the user submissions model so that it accepts every input in a compulsory fashion and doesn't leave out any of the optional fields. It also ensures that the user submissions model only accepts either free or paid plan names and not anything else.

--- app/models/user_submission.rb
+++ app/models/user_submission.rb
@@ -1,2 +1,4 @@
 class UserSubmission < ApplicationRecord
+    validates :first_name, :last_name, :email, :website, :job_role, :text, presence: true
+    validates :plan_name, inclusion: { in: %w(free paid) }
 end
