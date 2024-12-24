def1ebf00f1f1c8afc0fa193de4725800f1f8c16
Shreyas <shreyas314159@gmail.com>
2024-12-24 16:56:54 +0000

    added transactional mailers using a UserSubmissionMailer, So whenever the mailer wants to accept or reject a mail, there is a mail which is sent after that. So this is done by Daisy Chaining the accept or reject functions with the deliver function which comes baked into the Ruby on rails application

--- app/controllers/admin/user_submissions_controller.rb
+++ app/controllers/admin/user_submissions_controller.rb
@@ -5,15 +5,14 @@ class Admin::UserSubmissionsController < ApplicationController
 
     def update
         if @user_submission.update(user_submission_params)
-        redirect_to admin_path, notice: 'User submission updated successfully'
-        # TODO: Update user submissions status
+            redirect_to admin_path, notice: 'User submission updated successfully'
+            # Send mailer with accepted or rejected status
         else
         redirect_to admin_path, alert: 'Failed to update user submission'
         end
     end
 
     private
-
         # Accepts form inputs from the /apply page, anything else, we will separate concerns using an admin controller  
         def user_submission_params
             params.require(:user_submission).permit(:status)
@@ -22,4 +21,6 @@ class Admin::UserSubmissionsController < ApplicationController
         def set_user_submission
             @user_submission = UserSubmission.find(params[:id])
         end
+
+    
 end
--- app/models/user_submission.rb
+++ app/models/user_submission.rb
@@ -1,5 +1,13 @@
 class UserSubmission < ApplicationRecord
     validates :first_name, :last_name, :email, :website, :job_role, :text, presence: true
     validates :plan_name, inclusion: { in: %w(free paid) }
-    validates :status, inclusion: { in: %w(pending approved rejected) }
+
+    after_update :send_mailer
+
+    def send_mailer
+        puts "SENDING MAILER.."
+        UserSubmissionMailer.reject(self.email).deliver if status == 'reject'
+        UserSubmissionMailer.accept(self.email).deliver if status == 'accept'
+    end
+
 end
